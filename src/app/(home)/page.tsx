4851aea75c8c205f0787817da345c6df558445fc
yeliinbb <156063434+yeliinbb@users.noreply.github.com>
2024-07-04 22:23:48 +0900

    페이지네이션 로직으로 변경

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,9 +5,7 @@ const HomePage = () => {
   return (
     <div className="w-screen flex flex-col flex-1 items-center p-6 gap-2 overflow-auto">
       <h1 className="text-2xl font-bold">포켓몬 도감</h1>
-      <section className="w-[100%] h-[100%] p-6">
-        <PokemonList />
-      </section>
+      <PokemonList />
     </div>
   );
 };
--- src/app/(pokemons)/_components/PokemonList.tsx
+++ src/app/(pokemons)/_components/PokemonList.tsx
@@ -1,53 +1,36 @@
 "use client";
-import React from "react";
-import { useInfiniteQuery } from "@tanstack/react-query";
+import React, { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import Image from "next/image";
 import { Pokemon } from "@/types/types";
 import { AxiosError } from "axios";
 import Link from "next/link";
+import Pagination from "./Pagination";
 
 const ITEMS_PER_PAGE = 20;
 
 const PokemonList = () => {
-  const {
-    data: pokemons,
-    fetchNextPage,
-    hasNextPage,
-    isFetchingNextPage,
-    isPending,
-    error,
-    isSuccess,
-  } = useInfiniteQuery({
-    queryKey: ["pokemons"],
-    initialPageParam: 1,
-    queryFn: async ({ pageParam }) => {
-      console.log("pageParam", pageParam);
+  const [page, setPage] = useState<number>(1);
+  const { data, isPending, error, isSuccess } = useQuery({
+    queryKey: ["pokemons", page],
+    queryFn: async () => {
       const response = await axios.get("/api/pokemons", {
-        params: { page: pageParam, limit: ITEMS_PER_PAGE },
+        params: {
+          page: page,
+          limit: ITEMS_PER_PAGE,
+        },
       });
-      // console.log("API response", response);
-      // 여기서 리턴되는 데이터 값은 getNextPageParam의 인자 lastPage에 들어감.
+      // console.log("data", data);
       return {
         pokemons: response.data,
         totalPages: response.data.totalPages,
         hasNextPage: response.data.hasNextPage,
-        currentPage: pageParam,
       };
-    },
-    getNextPageParam: (lastPage, allPages, lastPageParam) => {
-      console.log("lastPage", lastPage); // 가장 최근에 불렀던 데이터
-      // console.log("lastPageParam", lastPageParam); // 가장 최근 페이지
-      // console.log("allPages", allPages); // 불러온 데이터 전체
-      return lastPage.hasNextPage ? lastPageParam + 1 : undefined;
-    },
-    select: ({ pages }) => {
-      // console.log("pages", pages);
-      return pages.flatMap((page) => page.pokemons.data);
     },
   });
 
-  console.log("pokemons", pokemons);
+  console.log("data", data);
 
   if (isPending) {
     return (
@@ -64,10 +47,10 @@ const PokemonList = () => {
   }
 
   return (
-    <>
-      <ul className="grid grid-cols-item-cards auto-rows-[200px] w-[100%] height-[100%] gap-4">
+    <section className="w-[100%] h-[100%] p-6">
+      <ul className="grid grid-cols-item-cards auto-rows-[200px] w-[100%] height-[100%] gap-4 min-h-[870px]">
         {isSuccess &&
-          pokemons.map((pokemon, index) => (
+          data.pokemons?.data?.map((pokemon, index) => (
             <Link href={`/pokemon/${pokemon.id}`} key={index}>
               <li className="flex flex-col items-center justify-center border-solid border-[1px] border-whitesmoke rounded-lg cursor-pointer hover:shadow-custom h-[200px]">
                 <Image
@@ -84,13 +67,9 @@ const PokemonList = () => {
               </li>
             </Link>
           ))}
-        {hasNextPage && (
-          <button onClick={() => fetchNextPage()} disabled={isFetchingNextPage}>
-            {isFetchingNextPage ? "로딩중..." : "더보기"}
-          </button>
-        )}
       </ul>
-    </>
+      <Pagination page={page} setPage={setPage} totalPages={data.totalPages} />
+    </section>
   );
 };
 
